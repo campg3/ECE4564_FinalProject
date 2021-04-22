f31f1b8b85ba70bf3af5277c8052201236141a79
Garrett Campbell <campg3@vt.edu>
2021-04-22 11:37:17 -0400

    added business file

---
+++ business.py
@@ -0,0 +1,5 @@
+# Names: Garrett Campbell, Bryan Yowler
+# Assignment: Final Project ECE 4564
+# File: business.py
+# Tool that businesses will use in order to scan QR codes,
+# send it to the government, and find out if QR code is valid
