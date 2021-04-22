5c40aa39f54d98d6d73b82e04bb5a46041e510e6
Bryan Yowler <ybryan10@vt.edu>
2021-04-22 12:03:47 -0400

    Created government files and basic Flask server

---
+++ gov_addinfo.py
@@ -0,0 +1,5 @@
+# Names: Garrett Campbell, Bryan Yowler
+# Assignment: Final Project ECE 4564
+# File: gov_addinfo.py
+# Purpose: Independent Python program used to insert vaccination information
+#          inot MongoDB database.
