36af5b48e6fd205e6fb13244cab2544342a76938
ybryan10 <ybryan10@vt.edu>
2021-04-29 09:47:57 -0400

    Initial program with GUI to insert user vaccination info into database

--- gov_addinfo.py
+++ gov_addinfo.py
@@ -2,4 +2,90 @@
 # Assignment: Final Project ECE 4564
 # File: gov_addinfo.py
 # Purpose: Independent Python program used to insert vaccination information
-#          inot MongoDB database.
+#          into MongoDB database.
+
+import tkinter as tk
+from pymongo import MongoClient
+import gov_keys
+from cryptography.fernet import Fernet
+from datetime import date
+import pickle
+
+# Initialize tk and variables
+root = tk.Tk()
+root.geometry("600x400")
+firstname_var = tk.StringVar()
+middlename_var = tk.StringVar()
+lastname_var = tk.StringVar()
+ssn_var = tk.StringVar()
+dob_var = tk.StringVar()
+
+# Connect to MongoDB Database
+client = MongoClient(
+        "mongodb+srv://" + gov_keys.DATABASE_ADMIN_USERNAME + ":" + gov_keys.DATABASE_ADMIN_PASSWORD +
+        "@cluster0.mm83g.mongodb.net/" +
+        gov_keys.DATABASE_NAME + "?retryWrites=true&w=majority")
+db = client[gov_keys.DATABASE_NAME]
+collection = db[gov_keys.PATIENT_COLLECTION]
+
+def submit():
+        # Get all fields from GUI
+        firstname = firstname_var.get()
+        middlename = middlename_var.get()
+        lastname = lastname_var.get()
+        ssn = ssn_var.get()
+        dob = dob_var.get()
+
+        # Create data for QR code and encrypt
+        qr_data = str(firstname + "_" + lastname + "_" + ssn[-4:])
+        gen_key = Fernet.generate_key()
+        key = Fernet(gen_key)
+        encrypted_qr = key.encrypt(qr_data.encode())
+
+        # Pickled QR encrypted data and key
+        unpickled = (encrypted_qr.decode(), gen_key.decode())
+        pickled_qr = pickle.dumps(unpickled)
+
+        # Add encryption entry to database
+        today = date.today()
+        current_date = today.strftime("%m/%d/%Y")
+        vaccine_entry = {"FirstName": firstname, "MiddleName": middlename, "LastName": lastname,
+                         "SSN": ssn, "DateOfBirth": dob, "QRCodeData": pickled_qr, "DateVaccinated": current_date,
+                         "NumBusinessRequests": 0}
+        x = collection.insert_one(vaccine_entry)
+
+        # Reset variables for next user to input
+        firstname_var.set("")
+        middlename_var.set("")
+        lastname_var.set("")
+        ssn_var.set("")
+        dob_var.set("")
+
+# Create labels and entry boxes for all fields
+firstname_label = tk.Label(root, text='First Name', font=('calibre', 12, 'bold'))
+firstname_entry = tk.Entry(root, textvariable=firstname_var, font=('calibre', 12, 'normal'))
+middlename_label = tk.Label(root, text='Middle Name', font=('calibre', 12, 'bold'))
+middlename_entry = tk.Entry(root, textvariable=middlename_var, font=('calibre', 12, 'normal'))
+lastname_label = tk.Label(root, text='Last Name', font=('calibre', 12, 'bold'))
+lastname_entry = tk.Entry(root, textvariable=lastname_var, font=('calibre', 12, 'normal'))
+ssn_label = tk.Label(root, text='Social Security Number (XXXXXXXXX)', font=('calibre', 12, 'bold'))
+ssn_entry = tk.Entry(root, textvariable=ssn_var, font=('calibre', 12, 'normal'))
+dob_label = tk.Label(root, text='Date of Birth (MM/DD/YYYY)', font=('calibre', 12, 'bold'))
+dob_entry = tk.Entry(root, textvariable=dob_var, font=('calibre', 12, 'normal'))
+submit_btn = tk.Button(root, text='Submit', command=submit)
+
+# Place entries and button in grid
+firstname_label.grid(row=0,column=0)
+firstname_entry.grid(row=0,column=1)
+middlename_label.grid(row=1,column=0)
+middlename_entry.grid(row=1,column=1)
+lastname_label.grid(row=2,column=0)
+lastname_entry.grid(row=2,column=1)
+ssn_label.grid(row=3,column=0)
+ssn_entry.grid(row=3,column=1)
+dob_label.grid(row=4,column=0)
+dob_entry.grid(row=4,column=1)
+submit_btn.grid(row=5,column=1)
+
+# Start GUI main loop
+tk.mainloop()
